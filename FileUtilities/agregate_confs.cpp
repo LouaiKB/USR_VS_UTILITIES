59022c1d97c17aeadd1a5da6e10b26dda7401706
LouaiKB <loukb1998@gmail.com>
2022-06-27 14:29:50 +0200

    get num mols from CLI

--- FileUtilities/agregate_confs.cpp
+++ FileUtilities/agregate_confs.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/fstream.hpp>
-#include <vector>
 
-using namespace std;
-using namespace boost::filesystem;
-
-template <typename T>
-inline vector<T> read(const path src)
-{
-	boost::filesystem::ifstream ifs(src, ios::binary | ios::ate);
-	const size_t num_bytes = ifs.tellg();
-	cout << "Reading " << src << " of " << num_bytes << " bytes!" << endl;
-	vector<T> buf;
-	buf.resize(num_bytes / sizeof(T));
-	ifs.seekg(0);
-	ifs.read(reinterpret_cast<char*>(buf.data()), num_bytes);
-	return buf;
-}
-
-int main(int argc, char* argv[])
-{
-	uint64_t agregated_confs = 0;
-	const size_t num_mols = 14646562; // for examples
-	for (size_t i = 0; i < num_mols; i++)
-	{
-		agregated_confs += 4;
-		cout.write(reinterpret_cast<char*>(&agregated_confs), sizeof(agregated_confs));
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr << "./agregate_confs [NUM_MOLECULES]" << std::endl;
+		return 1;
 	}
-	// to read the molecules
-	const auto mconfs = read<size_t>("mconfs.u64");
-	for (size_t i = 0; i < 100; i++)
-	{
-		cout << mconfs[i] << endl;
+	char* endPtr;
+	const uint64_t num_mols = strtoul(argv[1], &endPtr, 10);
+	uint64_t agregated_confs = 0;
+	for (uint64_t i = 0; i < num_mols; i++) {
+		agregated_confs += 4;
+		uint64_t* address = &agregated_confs;
+		const unsigned long size_buffer = sizeof(agregated_confs);
+		std::cout.write(reinterpret_cast<char*>(address), sizeof(size_buffer));
 	}
 }
